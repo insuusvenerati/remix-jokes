e8667e4167ea58efd75a1105c7c024916a6267e4
Sean Norwood <norwood.sean@gmail.com>
2021-12-16 07:42:46 -0600

    chore: add functions

---
+++ functions/index.js
@@ -0,0 +1,8 @@
+/**
+ * Send "Hello, World!"
+ * @param req https://expressjs.com/en/api.html#req
+ * @param res https://expressjs.com/en/api.html#res
+ */
+exports.helloWorld = (req, res) => {
+  res.send("Hello, World!");
+};
